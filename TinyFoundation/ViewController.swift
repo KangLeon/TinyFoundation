ce88b7dc4fc67cce0ea51c488f422e9bf1fc4e9f
jitengjiao <15048471585@163.com>
2021-03-02 16:42:39 +0800

    Initial Commit

---
+++ TinyFoundation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TinyFoundation
+//
+//  Created by 吉腾蛟 on 2021/3/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
