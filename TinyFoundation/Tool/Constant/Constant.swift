f3f6fa7bb40e8a3f82084f9c9a5948bfc6644863
jitengjiao <15048471585@163.com>
2021-03-09 13:47:44 +0800

    new
    new

--- TinyFoundation/Tool/Constant/Constant.swift
+++ TinyFoundation/Tool/Constant/Constant.swift
@@ -13,6 +13,8 @@ let HOST = "http://101.132.101.243/"
 let fund_list_url = "fund/all"
 //获取分页数据 参数：int page,int size
 let fund_list_page = "/fund/all/page"
-//搜索基金 参数 String key
+//搜索基金 参数： String key
 let fund_list_key = "/fund/key"
+//基金详情 参数：String code,Integer dateType 基金号、时间类型
+let fund_detail = "/fund/detail"
 
